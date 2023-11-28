6b231bf7957c7bca02ec7a0195bda9d04ddde234
Igor Batista Lima <igorbatistalima1@gmail.com>
2023-11-27 22:19:54 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
-const app = require('./app');
+const app = require('./app')
+const mongoose = require('mongoose');
+const { createAdminUser } = require('./models/Admin');
+
+
+
 
 app.listen(3000, () => {
     console.log('porta 3000');
