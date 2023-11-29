fe8ecbd0eb5ad2d7a27d90f94e6b46fe6c28ef2e
Igor Batista Lima <igorbatistalima1@gmail.com>
2023-11-28 23:43:08 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const routes = new Router();
 const UserController = require('./controllers/UserController');
 const {register, login} = require('./controllers/auth');
 const { authenticate } = require('./middlewares/auth');
-const { createAdminUser , Admin } = require('./models/Admin');
+const { createAdminUser , Admin, verifyAdmin } = require('./models/Admin');
 
 
 routes.get('/', async (req, res) => {
@@ -18,8 +18,29 @@ routes.post('/login', login);
 
 
 routes.get('/profile', authenticate, (req, res) => {
-    
+    res.json(req.user);
 });
+
+routes.put('/admin/alterar/:userId', authenticate, verifyAdmin, async (req, res) => {
+    try {
+      const user = await User.findById(req.params.userId);
+      const admin = await Admin.findById(req.params.userId);
+      if (!user && !admin) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      if (user) {
+        Object.assign(user, req.body);
+        await user.save();
+        return res.json(user);
+      }
+      Object.assign(admin, req.body);
+
+      await admin.save();
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+  });
 
 routes.post('/users', UserController.createUser);
 routes.get('/users/:username', UserController.getUser);
