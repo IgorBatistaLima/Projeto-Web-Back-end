eb4bcc8835a493ea6d1fa8615cb40dc4a8a6ea7d
Igor Batista Lima <igorbatistalima1@gmail.com>
2023-12-05 23:09:23 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,14 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+
+require('dotenv').config();
 
 const UserSchema = new Schema({
     username: {
         type: String,
         unique: true,
     },
-
     password: {
         type: String,
         required: true,
@@ -16,17 +17,32 @@ const UserSchema = new Schema({
         type: String,
         unique: true,
     },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
     role: {
         type: String,
         enum: ['user', 'admin'],
         default: 'user',
     },
-
 });
+
+
+async function createAdminUser() {
+    const adminUser = await User.findOne({ username: 'admin' });
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash('admin123', saltRounds);
+    
+    if (!adminUser) {
+      const admin = new User({
+        username: 'admin',
+        password: hashedPassword,
+        email: 'admin@example.com',
+        role: 'admin',
+      });
+      
+      admin.save()
+        .then(() => console.log('Admin user created'))
+        .catch(err => console.log('Error: ' + err));
+    }
+}
 
 UserSchema.methods.comparePassword = function (password) {
     return bcrypt.compare(password, this.password);
@@ -34,4 +50,4 @@ UserSchema.methods.comparePassword = function (password) {
 
 const User = mongoose.model('User', UserSchema);
 
-module.exports = User;
+module.exports = {User, createAdminUser};
