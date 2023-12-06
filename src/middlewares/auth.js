086f54698a3ae7d4e4b2a8b3f2814932808895f6
Igor Batista Lima <igorbatistalima1@gmail.com>
2023-12-05 23:09:30 -0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,40 +1,23 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const Admin = require('../models/Admin');
+require('dotenv').config();
 
-const authenticate = async (req, res, next) => {
+
+const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'No token provided' });
+    return res.status(401).send({ error: 'Token não enviado!' });
   }
 
   const token = authHeader.split(' ')[1];
 
-  if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
-  }
-
-  try {
-    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
-    let user;
-
-    if (decodedToken.role === 'admin') {
-      user = await Admin.findById(decodedToken._id);
-    } else {
-      user = await User.findById(decodedToken._id);
-    }
-
-    if (!user) {
-      console.log(token);
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    res.status(401).json({ message: 'Invalid token' });
-  }
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) return res.status(401).send({ error: 'Token inválido!' });
+    res.locals.auth_data = decoded;
+    console.log(decoded);
+    return next();
+  });
 };
 
-module.exports = { authenticate };
+
+module.exports = auth;
